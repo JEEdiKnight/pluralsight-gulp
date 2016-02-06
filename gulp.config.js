92c1f22690292a0934bc3976753f4b9aff9b50dc
Diego Gil <diego.gil@pucp.pe>
2016-02-06 15:02:44 -0600

    Removing Scripts and Styles from Main HTML and adding Bower Files and my Javascript to the HTML

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     var client = './src/client/';
-
+    var clientApp = client + 'app/'
     var config = {
         temp: './.tmp/',
         /**
@@ -11,7 +11,27 @@ module.exports = function() {
             './src/**/*.js',
             './*.js'
         ],
-        less: client + 'styles/styles.less'
+        client: client,
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+        less: client + 'styles/styles.less',
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        }
+    };
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
     };
     return config;
 };
